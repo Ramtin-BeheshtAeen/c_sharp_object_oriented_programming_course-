94cddf159f52f7cd126cbe9138ba89f85be37cd1
RamtinBeheshtAeen <ramtinba145822@gmail.com>
2023-11-24 15:17:04 +0330

    work on session 2.Entities:  (dotnet sln add session_02.UI.Console\session_02.UI.Console.csproj)  &(dotnet add session_02.UI.Console\session_02.UI.Console.csproj refrence session_02.Entities/session_02.Entities.csproj)

---
+++ session_2/session_02.UI.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿using session_02.Entities;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person p = new Person();
+        Console.WriteLine(p.ID);
+        Console.WriteLine(p.Name);
+    }
+}
